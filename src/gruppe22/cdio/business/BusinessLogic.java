d03f10fe7508adc9658de8d1f6a3707347a7a266
Anders Frandsen <anfran@anfran.dk>
2019-02-18 10:23:27 +0100

    setUserDTO in business logic

--- src/gruppe22/cdio/business/BusinessLogic.java
+++ src/gruppe22/cdio/business/BusinessLogic.java
@@ -6,4 +6,8 @@ import gruppe22.cdio.dal.UserDTO;
 public class BusinessLogic implements IBusinessLogic {
     private IUserDAO userDao;
     private UserDTO userDto;
+
+    public void setUserDTO(int userId) {
+        userDto = userDao.getUser(userId);
+    }
 }
--- src/gruppe22/cdio/business/IBusinessLogic.java
+++ src/gruppe22/cdio/business/IBusinessLogic.java
@@ -3,4 +3,5 @@ package gruppe22.cdio.business;
 public interface IBusinessLogic {
 
     //Todo: methods for logic and interaction with domain (DTO) goes here..
+    void setUserDTO(int userId);
 }
