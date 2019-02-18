72197d616484de841f2cca0e67e54e4fbb72bf79
killemille <s185128@student.dtu.dk>
2019-02-18 10:17:56 +0100

    Lavet metoder fra interfacet, bortset fra clearScreen()

---
+++ src/gruppe22/cdio/ui/TUI.java
@@ -0,0 +1,36 @@
+package gruppe22.cdio.ui;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//Skal tale med IController - metoderne skal kalde fra controlleren til sidst
+public class TUI implements IUserInterface{
+
+    void printLine(String text){
+        System.out.println(text);
+    }
+
+    void printList(ArrayList<String> array){
+        //Hvad skal array-navnet være - mere specifikt, userList?
+        System.out.println(array);
+    }
+
+    void clearScreen(){}
+
+    //Skal der være flere input - int vægt? - definere med forskellige meyode-navne
+    String getInput(){
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        scanner.close();
+        return userInput;
+    }
+
+    //Double getMachineInput(){
+    // Scanner scanner = new Scanner(System.in);
+    // Double weight = scanner.nextDouble();
+    // scanner.close();
+    // return weight;
+    // }
+
+    //Drop-down menu!
+}
