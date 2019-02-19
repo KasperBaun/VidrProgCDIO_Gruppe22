2141a4bab5b7be5567aba085872d557b371eaff2
Anders Frandsen <anfran@anfran.dk>
2019-02-19 14:09:10 +0100

    removed DAO from main

--- src/gruppe22/cdio/Main.java
+++ src/gruppe22/cdio/Main.java
@@ -4,14 +4,12 @@ import gruppe22.cdio.business.BusinessLogic;
 import gruppe22.cdio.business.IBusinessLogic;
 import gruppe22.cdio.controller.Controller;
 import gruppe22.cdio.controller.IController;
-import gruppe22.cdio.dal.IUserDAO;
 import gruppe22.cdio.ui.IUserInterface;
 import gruppe22.cdio.ui.TUI;
 
 public class Main {
 
     public static void main(String[] args) {
-        IUserDAO userDao;
         IBusinessLogic logic = new BusinessLogic();
         IUserInterface tui = new TUI();
         IController controller = new Controller();
