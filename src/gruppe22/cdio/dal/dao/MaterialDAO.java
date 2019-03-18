1cc267db9d334f4818cd4563e19119170bf2f934
Anders Frandsen <anfran@anfran.dk>
2019-03-18 14:10:53 +0100

    added material to DummyData

--- src/gruppe22/cdio/dal/dao/MaterialDAO.java
+++ src/gruppe22/cdio/dal/dao/MaterialDAO.java
@@ -1,11 +1,31 @@
 package gruppe22.cdio.dal.dao;
 
+import gruppe22.cdio.dal.DummyData;
+import gruppe22.cdio.dal.User;
 import gruppe22.cdio.dal.dto.MaterialDTO;
+import gruppe22.cdio.dal.dto.UserDTO;
+
+import java.io.IOException;
 
 public class MaterialDAO implements IMaterialDAO {
 
+    public MaterialDAO() throws IOException, ClassNotFoundException {
+    }
+
+    class DALException extends Exception {
+        public DALException(String msg, Throwable e) {
+            super(msg,e);
+        }
+        public DALException(String msg) {
+            super(msg);
+        }
+    }
+
+    private DummyData data = new DummyData();
+
     @Override
     public MaterialDTO getMaterial(int materialId) throws MaterialDTO.DALException {
-        return null;
+        MaterialDTO material = data.getMaterial(materialId);
+        return material;
     }
 }
